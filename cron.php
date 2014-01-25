bbf183e4bdb980801fa69056c3dc29f6b5cda308
friparia <friparia@gmail.com>
2014-01-25 17:49:12 +0900

    finish web and cron

--- cron.php
+++ cron.php
@@ -2,13 +2,14 @@
 $base = dirname(__FILE__); 
 require_once $base.'/Tieba.class.php';
 date_default_timezone_set("Asia/Shanghai");
-$data = (array)json_decode(file_get_contents('./storing.json'));
+$data = (array)json_decode(file_get_contents($base.'/storing.json'));
 
-$log = file_get_contents('./log');
+$log = file_get_contents($base.'/log');
+$result = array();
 foreach($data as $k=>$v){
     $tieba = new CTieba($v);
     $time = date("Y-m-d H:i:s");
     $result[$time] = $tieba->multisign();
 }
 $log .= json_encode($result);
-file_put_contents('./log', $log);
+file_put_contents($base.'/log', $log);
